f82843c44ebf6a7d7fd52266c7541309be383893
Viktor Warg <vw@hax.xyz>
2017-03-23 14:02:41 +0100

    PascalCase properties

--- StudentGroups/Database/Product.cs
+++ StudentGroups/Database/Product.cs
@@ -10,33 +10,33 @@ namespace HeftITGemer
     {
         public int ID { get; set; }
 
-        public string name { get; set; }
+        public string Name { get; set; }
 
-        public string short_description { get; set; }
+        public string ShortDescription { get; set; }
 
-        public string description { get; set; }
+        public string Description { get; set; }
 
-        public int parentProduct { get; set; }
+        public int ParentProduct { get; set; }
 
-        public float price { get; set; }
+        public float Price { get; set; }
 
-        public int countPerUnit { get; set; }
+        public int CountPerUnit { get; set; }
 
-        public int quantity { get; set; }
+        public int Quantity { get; set; }
 
-        public string comment { get; set; }
+        public string Comment { get; set; }
 
-        public string image { get; set; }
+        public string Image { get; set; }
 
-        public string video { get; set; }
+        public string Video { get; set; }
 
-        public int status { get; set; }
+        public int Status { get; set; }
 
-        public int manufacturerID { get; set; }
+        public int ManufacturerID { get; set; }
 
-        public string manufacturer_productnumber { get; set; }
+        public string ManufacturerProductNumber { get; set; }
 
-        public int categoryID { get; set; }
+        public int CategoryID { get; set; }
 
     }
 }
