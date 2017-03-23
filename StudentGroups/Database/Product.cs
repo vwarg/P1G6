d9b35b06f95e640195f7d2cad1ea0daa78166f6c
torselden <tor.selden@gmail.com>
2017-03-23 14:26:28 +0100

    Added constructor to Product-class taking all parameters

--- StudentGroups/Database/Product.cs
+++ StudentGroups/Database/Product.cs
@@ -9,34 +9,43 @@ namespace HeftITGemer
     public class Product
     {
         public int ID { get; set; }
-
         public string Name { get; set; }
-
         public string ShortDescription { get; set; }
-
         public string Description { get; set; }
-
         public int ParentProduct { get; set; }
-
         public float Price { get; set; }
-
         public int CountPerUnit { get; set; }
-
         public int Quantity { get; set; }
-
         public string Comment { get; set; }
-
         public string Image { get; set; }
-
         public string Video { get; set; }
-
         public int Status { get; set; }
-
         public int ManufacturerID { get; set; }
-
         public string ManufacturerProductNumber { get; set; }
-
         public int CategoryID { get; set; }
 
+        //Konstruktor för alla parametrar i klassen       
+        public Product(int id, string name, string shortDescription, string description, int parentProduct, float price, int countPerUnit, int quantity, string comment, string image, string video, int status, int manufacturerID, string manufacturerProductNumber, int categoryID)
+        {
+            ID = id;
+            Name = name;
+            ShortDescription = shortDescription;
+            Description = description;
+            ParentProduct = parentProduct;
+            Price = price;
+            CountPerUnit = countPerUnit;
+            Quantity = quantity;
+            Comment = comment;
+            Image = image;
+            Video = video;
+            Status = status;
+            ManufacturerID = manufacturerID;
+            ManufacturerProductNumber = manufacturerProductNumber
+            CategoryID = categoryID;
+        }
+
     }
+
+
+
 }
