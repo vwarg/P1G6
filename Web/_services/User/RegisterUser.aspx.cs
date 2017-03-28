ee29dcb5b685d48b00a01d490e58654b7b8156f4
Administrator <Administrator@ACADEMY-6211VQH>
2017-03-28 15:55:15 +0200

    RegisterUser fix'd? extra konstruktor i user + fix adduserinfo

--- Web/_services/User/RegisterUser.aspx.cs
+++ Web/_services/User/RegisterUser.aspx.cs
@@ -1,4 +1,5 @@
-﻿using HeftITGemer;
+﻿using EshopSQL;
+using HeftITGemer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +20,13 @@ namespace Web._services.User
             if (Session["User"] != null)
             {
                 HeftITGemer.User u = (HeftITGemer.User)Session["User"];
-                 User user = new User(Request.Form["firstname"], Request.Form["lastname"], Request.Form["phone"], Request.Form["companyname"], -1, -1); //adress FÅR EJ vara -1. MÅSTE ha ID tack vare FK.
-                int uiid = UserInfo.AddUserInfo(userinfo);
-                u.Contactinfo = uiid;
+
+                int uid = SQL.AddUser(Request.Form["email"], Request.Form["password"], Convert.ToInt32(Request.Form["contactInfo"]));
+
+                u.ID = uid;
+
                 Session["User"] = u;
-                Response.Write($"{userinfo.ID}\r\n");
+                Response.Write($"{uid} \r\n");
                 Response.StatusCode = 200;
                 Response.End();
             }
