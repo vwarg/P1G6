5a2e340d36192cd71f31d93f99c95bffea64a411
Viktor Warg <vw@hax.xyz>
2017-03-28 13:36:49 +0200

    Add PagedGet

--- Web/js/ProductManager.js
+++ Web/js/ProductManager.js
@@ -22,6 +22,28 @@ function GetProducts() {
           });
 }
 
+function PagedGet(productsPerPage, offset) {
+    var jqxhr = $.getJSON("/_services/Products/GetProducts")
+      .done(function (data) {
+          RenderProductsPaged(data, productsPerPage, offset);
+      })
+      .fail(function () {
+          console.log("OOPS! (GetProducts())");
+      });
+}
+
+function RenderProductsPaged(productsArray, perPage, offset) {
+    var parent = $(".containerFourItems");
+    for (var i = offset; i < Math.min(offset+perPage, productsArray.length); i++) {
+        var imageUrl = (productsArray[i].Image ? productsArray[i].Image : "/img/products/binder.png");
+        var name = productsArray[i].Name;
+        var price = productsArray[i].Price.replace(",", ".");
+        var divContent = '<div class="productBox"><img src="' + imageUrl + '" alt="" /><p class="productName">' + name + '</p><p class="productPrice">' + parseFloat(price).toFixed(2).replace(".", ",") + ':-</p><form>';
+        divContent += '<select name="variation" disabled><option selected="selected" disabled>...</option></select><input type="submit" name="addToCart" value="+" /></form></div>';
+        parent.html(parent.html() + divContent);
+    }
+}
+
 
 /*
 <div class="containerFourItems">
