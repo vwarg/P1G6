2c0bc29839196887634ad302c07bab9bbba80ac4
Viktor Warg <vw@hax.xyz>
2017-03-28 11:53:29 +0200

    WIP -- Begin work on RegisterUser

---
+++ Web/_services/User/RegisterUser.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web._services.User
+{
+    public partial class RegisterUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //User:: Request.Form["email"], Request.Form["password"]
+            //UserInfo:: se AddUserInfo
+            //Adress:: _minst_ en, se Adress();
+        }
+    }
+}
