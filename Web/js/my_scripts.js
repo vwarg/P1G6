da6771eabc4d4ef4a33d003c7dfc5136493a8ca0
Jonas Jilmefors <jonas.jilmefors@hotmail.com>
2017-03-28 16:41:30 +0200

    CSS fix

--- Web/js/my_scripts.js
+++ Web/js/my_scripts.js
@@ -267,4 +267,21 @@ $(document).ready(function () {
 		$('#toggleLogin').show();
 		$('#toggleRegister').show();
 	});
+
+    // Fake subscription actions
+	$('#subscribeBox form').on('submit', function () {
+	    var userEmail = $('#subscribeBox input[name=subscriptionEmail]').val();
+	    if (isValidEmailAddress(userEmail)) {
+	        $('#subscribeBox form').html('<p>Nyheter kommer att skickas till ' + userEmail + '</p>');
+	    }
+	    else {
+	        $('#subscribeBox form').append('<p>Ange en giltig e-postadress.</p>');
+	    }
+	    return false;
+	});
+
+	function isValidEmailAddress(emailAddress) {
+	    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+	    return pattern.test(emailAddress);
+	};
 });
